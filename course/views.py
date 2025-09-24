7a79f8ee35190caed35fb02b4130bbd81fac2b1f
sashbeloov <beloovsasha23@gmail.com>
2025-09-24 17:20:05 +0500

    updated content section it works

--- course/views.py
+++ course/views.py
@@ -7,31 +7,29 @@ from .serializers import LanguageSerializer, TopicSerializer, ContentSerializer
 
 def language_view(request, language_slug):
     language = get_object_or_404(Language, slug=language_slug)
-    # topics = language.topics.all().order_by("order")
-    topics = language.topics.exclude(slug__isnull=True).exclude(slug="").order_by("order")
-
-    print("Topics in view:", [(t.id, t.title, t.slug) for t in topics])
+    topics = language.topics.all().order_by("order")
 
     return render(request, "topic.html", {
         "language": language,
         "topics": topics,
     })
 
-
-def content_view(request, language_slug, topic_slug=None):
+def content_view(request, language_slug, topic_slug):
+    # Tilni topamiz
     language = get_object_or_404(Language, slug=language_slug)
+
+    # Barcha mavzular chap menyu uchun
     topics = language.topics.all().order_by("order")
-    
-    # Agar topic_slug berilgan bo'lsa, shu topicni olamiz
-    topic = None
-    contents = []
-    if topic_slug:
-        topic = get_object_or_404(Topic, language=language, slug=topic_slug)
-        contents = topic.contents.all().order_by("order")
-    
+
+    # Hozirgi topic
+    current_topic = get_object_or_404(Topic, slug=topic_slug, language=language)
+
+    # Shu topicga tegishli contentlar
+    contents = current_topic.contents.all().order_by("order")
+
     return render(request, "content.html", {
         "language": language,
-        "topics": topics,
-        "current_topic": topic,
+        "topics": topics,              # chap menyu uchun
+        "current_topic": current_topic, # shablonda ishlatyapsan
         "contents": contents,
     })
